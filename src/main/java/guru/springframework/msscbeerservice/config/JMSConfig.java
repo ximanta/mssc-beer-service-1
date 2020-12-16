a54a838d62988dd9c5c22d4b3c1c9a203b7116a0
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2020-12-16 17:38:57 -0600

    Added listener to inform Inventory service to create new beers upon brewing request

--- src/main/java/guru/springframework/msscbeerservice/config/JMSConfig.java
+++ src/main/java/guru/springframework/msscbeerservice/config/JMSConfig.java
@@ -9,6 +9,7 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JMSConfig {
     public static final String BREWING_REQUEST_QUEUE = "brewing-request";
+    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
 
     @Bean
     public MessageConverter messageConverter() {

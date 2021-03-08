c6d18057bce3e42c506e4b60149c31f37b4b4b5a
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2021-03-08 14:29:18 -0600

    Added eureka client config, but does not work. Bugged

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-04T17:19:37-0600",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 12.0.1 (Oracle Corporation)"
+    date = "2021-03-08T14:14:28-0600",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.10 (Oracle Corporation)"
 )
 @Component
 @Primary

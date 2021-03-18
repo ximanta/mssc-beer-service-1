e158e94ea38a892289a355a7e8d5af78b9d276b1
Ximanta <ximanta.sarma@gmail.com>
2021-03-18 08:49:22 +0530

    Fixed eureka discovery

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -3,9 +3,11 @@ package guru.springframework.msscbeerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsscBeerServiceApplication {
 
     public static void main(String[] args) {
--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-08T14:14:28-0600",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.10 (Oracle Corporation)"
+    date = "2021-03-17T11:55:01+0530",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.1 (Oracle Corporation)"
 )
 @Component
 @Qualifier("delegate")
